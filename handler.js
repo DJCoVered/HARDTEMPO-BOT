40f75b87605864c75b7361a14cc33c1a0b5ee740
DJCoVered <134423410+DJCoVered@users.noreply.github.com>
2023-06-08 15:05:17 +0200

    Update handler.js

--- handler.js
+++ handler.js
@@ -338,7 +338,7 @@ if (xp > 2000)
 m.reply('Exp limit') // LÍMITE DE EXP
 else               
 if (!isPrems && plugin.money && global.db.data.users[m.sender].money < plugin.money * 1) {
-this.reply(m.chat, `🐈 *NO TIENE GATACOINS*`, m)
+this.reply(m.chat, `🐈 *NO TIENE HARDCOINS*`, m)
 continue // LÍMITE DE EXP    
 }
 m.exp += xp
@@ -382,7 +382,7 @@ if (m.limit)
 m.reply(+m.limit + lenguajeGB.smsCont8())
 }
 if (m.money)
-m.reply(+m.money + ' *GATACOINS USADO(S)* 🐱')
+m.reply(+m.money + ' *HARDCOINS USADO(S)* 🐱')
 break
 }}} catch (e) {
 console.error(e)
